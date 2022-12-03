818a4e66ace5a9f0d7a989e72ac194abd2e632aa
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-02 22:12:10 -0500

    Add navigation bar to the app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require jquery
-//= require bootstrap-sprockets
 
 import "@hotwired/turbo-rails"
 import "controllers"
 
+//= require jquery
+//= require bootstrap-sprockets
