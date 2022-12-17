a845ae66ef4a26467d534d990f6c8331b928a625
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-16 22:17:18 -0500

    Configure and create helpers -Change include_all_helpers config to false -Split the collapsible_elements.html.erb file's content into partials and extract logic from the file into partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+require 'navigation_helper'
+
 module ApplicationHelper
+  include NavigationHelper
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Collabfield
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.action_controller.include_all_helpers = false
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
